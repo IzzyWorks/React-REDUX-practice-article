a73dc352080c24ba0dfd773b286a55be54c93c6e
Izzy <izzy.aspler@gmail.com>
2021-10-07 12:14:44 -0700

    added redux, and wrapped app in a provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import Post from './components/Post';
 import PostForm from './components/Postform';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <PostForm />
-        <hr />
-        <Post />
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <header className='App-header'>
+          <PostForm />
+          <hr />
+          <Post />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
