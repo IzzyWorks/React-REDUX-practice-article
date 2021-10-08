841e84ab4816a5986408a61489e1f4e7f2e3dcb1
Izzy <izzy.aspler@gmail.com>
2021-10-07 23:14:59 -0700

    import error: thunk is not exported from redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import Post from './components/Post';
+import Posts from './components/Posts';
 import PostForm from './components/Postform';
 
-const store = createStore(() => [], {}, applyMiddleware());
+import store from './store';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className='App'>
-        <header className='App-header'>
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>Welcome to React</h1>
+          </header>
           <PostForm />
           <hr />
-          <Post />
-        </header>
-      </div>
-    </Provider>
-  );
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
